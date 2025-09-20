0670013588ff057552932f473225c53f8c934053
田心61 <2737504027@qq.com>
2025-09-20 14:27:34 +0800

    Create my.h
    客户端与服务器需要的头文件

---
+++ my.h
@@ -0,0 +1,21 @@
+#ifndef _MY_H
+#define _MY_H
+
+//避免头文件重复包含的逻辑
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <pthread.h> // process thread 进程线程
+#include <semaphore.h> //semaphore 信号量
+#include <signal.h> //signal 信号
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sqlite3.h>
+#include <sys/epoll.h>
+#endif
